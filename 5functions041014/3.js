b44957342646fc109ab9fd18fd1b4cb6d64e1674
Tamas <puskitamas@gmail.com>
2014-10-05 15:12:23 +0200

    functional#3

--- 5functions041014/3.js
+++ 5functions041014/3.js
@@ -1,16 +1,17 @@
 var school = [1, 2, 3, 4, 5, 6, 7, 8, 3, 35, 3, 5];
 
-Array.prototype.map = function(student) {
-	var result = [];
+
+var result = [];
+
+Array.prototype.map = function(grade) {
+	//var result = [];
 	this.forEach(function(item) {
-		result.push(student(item));
+		result.push(grade(item));
 	});
 	return result;
 };
+console.log(result);
 
-
-/*var doubles = school.map(function(num){
-	return num * 2;
-});
-
-console.log(doubles);*/
+/**var doubles = school.map(function(num)
+ * {return num * 2; }); console.log(doubles);
+ */
---
+++ 5functions041014/5.js
@@ -0,0 +1,35 @@
+var zoo = [{
+	id: Math.floor(Math.random() * 10000000).toString(),
+	species: 'Mammal',
+	type: 'Rabbit',
+	name: 'BugsBunny',
+	food: 'Carrot',
+}, {
+	id: Math.floor(Math.random() * 10000000).toString(),
+	species: 'Gilled',
+	type: 'Fish',
+	name: 'Nemo',
+	food: 'Alga',
+}, {
+	id: Math.floor(Math.random() * 10000000).toString(),
+	species: 'Mammal',
+	type: 'Fox',
+	name: 'Groovy',
+	food: 'Bird',
+}, {
+	id: Math.floor(Math.random() * 10000000).toString(),
+	species: 'Mammal',
+	type: 'Bear',
+	name: 'John',
+	food: 'Strawberry',
+}];
+
+var specials = [];
+
+var newRow = zoo.forEach(function(animal) {
+	if (animal.id % 4 === 0) {
+		specials.push(animal);
+	}
+	return newRow;
+});
+console.log(specials);
