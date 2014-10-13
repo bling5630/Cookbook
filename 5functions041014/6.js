f712d3e9538c56821c6a0affcf89ca3dcef5e260
Tamas <puskitamas@gmail.com>
2014-10-13 20:14:53 +0200

    fuzzy#3

--- 5functions041014/6.js
+++ 5functions041014/6.js
@@ -23,3 +23,21 @@ var zoo = [{
 	name: 'John',
 	food: 'Strawberry',
 }];
+
+var school = [1, 2, 3, 4, 5, 6, 7, 8, 3, 35, 3, 5];
+
+Array.prototype.filter = function(grade) {
+	var result = [];
+	this.forEach(function(item) {
+		if (grade(item)) {
+			result.push(item)
+		}
+	})
+	return result;
+}
+
+var a = school.filter(function(x) {
+	return x > 3 && x < 8
+})
+
+console.log(a)
