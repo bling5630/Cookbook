98867d7e6652d8120375755ccca20016e0603c13
Tamas <puskitamas@gmail.com>
2014-12-07 20:13:32 +0100

    initial

---
+++ exp4/README.md
@@ -0,0 +1,7 @@
+Creating RESTful APIs with Express 4
+
+by
+
+Sandeep Panda - @Sandeepg33k
+
+http://www.sitepoint.com/creating-restful-apis-express-4/
---
+++ exp4/app.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var morgan = require('morgan');
+var movies = require('./routes/movies');
+var mongoose = require('mongoose');
+
+var app = express();
+
+
+var dbName = 'movieDB',
+connectionString = 'mongodb://localhost27017' + dbName;
+
+mongoose.connect(connectionString);
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(morgan("dev"));
+app.use('/api', movies); //This is our route middleware
+ 
+module.exports = app;
