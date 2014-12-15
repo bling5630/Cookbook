b4ab314d53a2db9fdf69ed4887b468b69baec687
Tamas <puskitamas@gmail.com>
2014-12-15 22:10:33 +0100

    #13

--- nodetuts/tcp/read.js
+++ nodetuts/tcp/read.js
@@ -1,41 +1,69 @@
 // 19:30
 
 var net = require('net'),
-	JSONStream = require('json-stream');
+	JSONStream = require('json-stream'),
+	es = require('event-stream'),
+	pup = require('pup');
+
+function alwaysRetrunTrue() {
+	return true;
+}
 
 module.exports = function(source) {
 	var server = net.createServer();
 
 	server.on('connection', function(socket) {
 		var started = false;
+		var filter;
 
 		var stringifier = es.mapSync(function(data) {
 			return JSON.stringify(data) + '\n';
 		});
 
-		// {"action" : "start"}
-		// {"action" : "stop"}
+		// {"action": "start", "filter": "return ev.a && ev.a.charaT(0) == 'A';""}
+		// {"action": "stop"}
+
+		function end() {
+			pup.unpipe(source, filter);
+			pup.unpipe(filter, stringifier);
+			pup.unpipe(stringifier);
+			started = false;
+		}
 
 		var actions = {
 			start: function(command) {
 				if (started) return;
 
-				source
-					.pipe(stringifier)
-					.pipe(socket);
+				if (command.filter) {
+					command.filter = new Function('ev', command.filter);
+				}
+
+				if (!command.filter) command.filter = alwaysRetrunTrue;
+
+				filter = es.mapSync(function(event) {
+					if (command.filter(event)) return event;
+				});
+
+				pup.pipe(source, filter);
+				pup.pipe(filter, stringifier);
+				pup.pipe(stringifier);
 
 				started = true;
-			}
+			},
+
+			stop: end
 		};
 
 		var jsonStream = new JSONStream();
 
-		jsonStream.('data', function(command) {
+		jsonStream.on('data', function(command) {
 			var action = command.action;
 			if (action && actions[action]) actions[action](command);
 		});
 
 		socket.pipe(jsonStream);
+
+		socket.on('end', end);
 	});
 	server.listen(4001);
 };
