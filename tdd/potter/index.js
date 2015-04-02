24de9d43be97f754d948a3e3b5b92ef7f4f65dc3
Tamas <puskitamas@gmail.com>
2015-04-02 21:46:40 +0200

    kataPotter#2

--- tdd/potter/index.js
+++ tdd/potter/index.js
@@ -1,11 +1,30 @@
 // kataPotter
 
-var BASE_PRICE = 8;
+var BASE_PRICE = 8,
+	BOOK_LIST = [1, 0, 0, 0, 0],
+	DISCOUNTS = {
+		'1': 1,
+		'2': 0.95,
+		'3': 0.90,
+		'4': 0.80,
+		'5': 0.75
+	};
 
 function kataPotter(book, price) {
 	return book * price;
 }
 
+function getDiscount(bookDiscount) {
+	return bookDiscount === 0 && 0 ||
+		bookDiscount === 1 && 0 ||
+		bookDiscount === 2 && 0.95 ||
+		bookDiscount === 3 && 0.9 ||
+		bookDiscount === 4 && 0.8 ||
+		bookDiscount === 5 && 0.75;
+}
+
+
 console.log(kataPotter(8, 3));
+console.log(BOOK_LIST[0]);
 
 module.exports = kataPotter;
