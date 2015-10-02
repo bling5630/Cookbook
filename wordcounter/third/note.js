46c4d7e37f5f5819cb605bba1eff1c5122cb8c99
bling5630 <puskitamas@gmail.com>
2015-10-02 21:15:44 +0200

    some_small_fix

--- wordcounter/third/note.js
+++ wordcounter/third/note.js
@@ -3,7 +3,7 @@ heroku, van egy szoveg mezo s egy generate gomb,
 be rakom a linket, csigabiga, s jon a d3
 
 szepen ugy mukodik, hogy ha megyek az oldalra
-puski.heroku.com, van egy node,js alkalmazas
+puski.heroku.com, van egy node, js alkalmazas
 kikop egy html file t, ezt sajat file ok kozul olvassa ki
 van egy repo ahonnan kiveszi, lesz egy doboz, ehhez
 tartozik majd egy kliens oldali js, lesz js, fut
@@ -12,8 +12,8 @@ kliens szerverbe fogunk irni, elkuldi majd a linket, s meg fogja
 varni a node js szerver h valaszoljon a szavak listajara
 
 ugyanez a nodejs kliens bevarja a d3 mat s kirajzolja a buborekot
-nodejs, server, express, url elekre fog valaszolni//// lesznek
-/ akarmi akkor akarmit fog hivni, a wordcount/?url valami lesz
+nodejs, server, express, url elekre fog valaszolni //// lesznek
+  / akarmi akkor akarmit fog hivni, a wordcount / ? url valami lesz
 akkor ki fogja szamolni az izeket, lekeri az oldalt, vegigfuttatja
 az algoritmusokat, blablabla
 
@@ -21,15 +21,15 @@ most oda kene eljutni, hogy a legeleljen, legyen nodejs szerver, local
 kiszolgala egy html file, van szovegdoboz,
 
 express, app.static, ha megadok egy konyvtar nevet, ahol az express futtatom,
-static konyvtarat, belerakok egy html t,
+  static konyvtarat, belerakok egy html t,
 
-legyen h fut az express, localhost3000/index.html
-szovegbox+ gomb, , html be huzzak be js file t,
-script tag kell, aminek van egy src attributuma,
-ahova js filet teszek be, az eleresi utjat
+  legyen h fut az express, localhost3000 / index.html
+szovegbox + gomb, , html be huzzak be js file t,
+  script tag kell, aminek van egy src attributuma,
+  ahova js filet teszek be, az eleresi utjat
 static mappa, ha melle rakom a index.html melle rakom a script.js
-s az index html beleirom scriptkcscsor, src/
-head vagy body, legyen alert, ha mukodik akkor jon egy alert
+s az index html beleirom scriptkcscsor, src /
+  head vagy body, legyen alert, ha mukodik akkor jon egy alert
 
 magamtol, jquery vagy barmivel,
 akkor irja ki h alert, ha rakattintok a gomba
@@ -37,3 +37,31 @@ akkor irja ki h alert, ha rakattintok a gomba
 ket kulombozo dolog
 a alert et beirni
 b amikor a gombnyomasra mukodik
+
+-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+-- -- -- -- -- -
+
+// callback, nehez dolog, van egy file, van egy kod, fut,
+// ha nincs benne vegtelen ciklus vagy valami akkor abbahagyja
+// egyszeru file, for 0-100, cl penis, egy ideig lefut
+// vannak olyan dolgok, elsore bongeszo, kattintok
+// huzom az egeret, ilyesmi, a js valaszoljon dolgokra
+// az tortent, js futogat, s ha van egy esemeny, akkor ez be tud szurni
+// kodreszletet, inkabb az van, hogy ha tudja a js
+// h vannak helyzetek amire figyelni kell, akkor nem allitja meg
+// a futast akkor idokozonkent megnezni, hogy mi tortenik,
+// ez az event loop, idonkent megnezni, hogy mi van
+ha tudja h valamire figyelnie kell akkor nem hagyja abba a futast,
+hanem nezi
+egyszerubb dolog, settimeout, elso parameter fn, masodik 1000 milisekundum
+1000 miliszekundumonkent beszurja a fn, akkor beszurja a fuggvenyt, amit ott mondtam ott neki
+az epp nem csinaltam az jelenti, hogy irtam egy vegtelen ciklust, ha vegtelen a dolog akkor
+nincs ideje kinezni a pontokon hogy vartam e valamit,
+
+callback, lefuttatok egy fuggvenyt, s ha nem tortenik semmi akkor tortenjen valami
+
+async nem varja meg h kinyitja a filet, sync megvarja mig kinyitja a filet
+
+
+learnyounode
+javascripting
