311ae833850a5cb7c1b0582ec8208f37f8d5eaaa
bling5630 <puskitamas@gmail.com>
2015-10-10 11:53:52 +0200

    rest_api_is back

--- testing/supertest/marcus_oft/posttest.js
+++ testing/supertest/marcus_oft/posttest.js
@@ -1,11 +1,14 @@
 var request = require('supertest');
 
-//var app = require('./postApp').postApp;
+var app = require('./postApp').postApp;
 
-describe("Posting is easy to test with supertest", function (){
+describe("Posting is easy to test with supertest", function() {
 
-  it("posts a new user to /users", function(done){
-    var user = { username : 'marcus', email : 'marcus@marcus.com'};
+  it("posts a new user to /users", function(done) {
+    var user = {
+      username: 'marcus',
+      email: 'marcus@marcus.com'
+    };
 
     request("http://localhost:3000")
       .post("/users")
--- testing/supertest/shaky_shane/test/index.spec.js
+++ testing/supertest/shaky_shane/test/index.spec.js
@@ -1,13 +1,1 @@
-var assert = require("chai").assert;
-var http = require("http");
-var server = require("../src/index");
 
-it("should return a 200 response", function(done) {
-
-  var app = server();
-
-  http.get("http://localhost:8000", function(res) {
-    assert.equal(res.statusCode, 200);
-    done();
-  });
-});
