3e4e79748ecb766685657747332be9a84251ffd4
bling5630 <puskitamas@gmail.com>
2015-12-26 20:32:33 +0100

    ramda_transpose_spiral

--- functional/spiral_matrix/test/five_transpose.js
+++ functional/spiral_matrix/test/five_transpose.js
@@ -13,9 +13,48 @@ var transpose_one = list =>
     _.map(row => _.nth(index)(row), list), _.head(list)
   );
 
+var transpose_two = list =>
+    _.addIndex(_.map)((column, index) =>
+    _.map(_.nth(index), list), _.head(list)
+  );
 
+var transpose_three = list =>
+    _.addIndex(_.map)(_.pipe(_.nthArg(1),
+    _.nth,
+    _.map(_.__, list)),
+    _.head(list)
+  );
 
+var transpose_four = list =>
+    _.reduce(_.zipWith(_.flip(_.append)),
+    _.repeat([], _.apply(Math.min, _.map(_.length, list))),
+    list
+    );
 
+var transpose_five = _.lift(_.reduce(_.zipWith(_.flip(_.append))))(
+   _.pipe(_.map(_.length),
+   _.apply(Math.min),
+   _.repeat([])),
+   _.identity
+);
 
+var BASE_SHORTEST = _.compose(
+  _.apply(Math.min),
+  _.map(_.length)
+);
 
-console.log(transpose_one(BASE_DATA));
+var transpose_six = _.lift(_.reduce(_.zipWith(_.flip(_.append))))(
+   _.compose(_.repeat([]), BASE_SHORTEST),
+   _.identity
+);
+
+var BASE_RESULT = [
+    transpose_one(BASE_DATA),
+    transpose_two(BASE_DATA),
+    transpose_three(BASE_DATA),
+    transpose_four(BASE_DATA),
+    transpose_five(BASE_DATA),
+    transpose_six(BASE_DATA)
+];
+
+console.log(BASE_RESULT);
