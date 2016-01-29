1227716b52d69d15ca2adb5ca4b91727541c67f1
bling5630 <puskitamas@gmail.com>
2016-01-29 19:58:36 +0100

    small_d3

--- functional/ramda_practice/done/prototype.js
+++ functional/ramda_practice/done/prototype.js
@@ -1,3 +1,3 @@
-var tool = require('./assoc');
+var tool = require('./');
 
-console.log(tool('c', 3, {a: 1, b: 2})); //=> {a: 1, b: 2, c: 3}
+console.log();
--- rest_api/submit_and_load/readme.md
+++
@@ -1,2 +0,0 @@
-[reference](http://www.infocaptor.com/bubble-my-page)
-[D3 Bubble chart](http://bl.ocks.org/phuonghuynh/54a2f97950feadb45b07)
