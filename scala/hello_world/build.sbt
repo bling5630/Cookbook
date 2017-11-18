ecd418ffb2944c8e3ebba411e7c737721c31e4aa
bling5630 <puskitamas@gmail.com>
2017-11-18 16:02:05 +0100

    scala hello world

---
+++ scala/hello_world/build.sbt
@@ -0,0 +1,6 @@
+name := "trial"
+
+version := "0.1"
+
+scalaVersion := "2.12.4"
+        
---
+++ scala/hello_world/src/main/scala/scalaApp.scala
@@ -0,0 +1,5 @@
+object scalaApp {
+  def main(args: Array[String]) {
+    println("Hello, world! and people")
+  }
+}
